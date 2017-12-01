f755d3b5405ee745a10deee2e6debc65d117b423
kawax <kawaxbiz@gmail.com>
2017-12-01 21:03:00 +0900

    update

--- enable-automatic-updates-vcs.php
+++ enable-automatic-updates-vcs.php
@@ -10,4 +10,4 @@ License:         GPLv2 or later
 License URI:     http://www.gnu.org/licenses/gpl-2.0.html
 */
 
-add_filter( 'automatic_updates_is_vcs_checkout', '__return_true', 1 );
+add_filter( 'automatic_updates_is_vcs_checkout', '__return_false');
