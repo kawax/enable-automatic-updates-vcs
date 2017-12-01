708d80d1edabef7c03a7d4d3f1b3395336558a01
kawax <kawaxbiz@gmail.com>
2017-12-01 20:51:18 +0900

    Enable Automatic Updates for VCS

---
+++ README.md
@@ -0,0 +1,6 @@
+# Enable Automatic Updates for VCS
+
+WordPress はデフォルトでは `.git` が存在すると自動更新されないので更新できるようにするプラグイン。  
+1行で済むことだけど全部のテーマに書くのは大変なのでプラグイン化。  
+WP公式には登録せず composer から使う前提。  
+mu-plugin なのでインストールすれば常に有効。
---
+++ enable-automatic-updates-vcs.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Enable Automatic Updates for VCS
+Plugin URI:
+Description:
+Version: 1.0.0
+Author: kawax
+Author URI:
+License:         GPLv2 or later
+License URI:     http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+add_filter( 'automatic_updates_is_vcs_checkout', '__return_true', 1 );
